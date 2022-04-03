272dfcf29eb2202d25f3aec36c8100bfaf3e5a10
Anket pratap singh <72145331+ThakurAnketPratapSingh@users.noreply.github.com>
2022-04-03 17:13:57 +0530

    Add files via upload

---
+++ April/week1/Assignment13/task2.js
@@ -0,0 +1,17 @@
+var a,b,c;
+a=10;
+b=50;
+c=30;
+if(a > b && a > c){
+    console.log("a is Greater than b and c");
+}
+else if (b > a && b > c){
+    console.log("b is Greater than a and c");
+}
+else if (c > a && c > b){
+    console.log("c is Greater than a and b");
+}
+else{
+    console.log("all are equal or any two values are equal");
+}
+
